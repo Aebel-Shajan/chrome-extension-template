725dd4910fbd299e45acea7c702fb0318cbd1dcc
aebel-shajan <aebelshajan@gmail.com>
2024-11-04 15:25:19 +0000

    Add src path alias "@"

--- vite.config.base.ts
+++ vite.config.base.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +14,11 @@ export default defineConfig({
         chunkFileNames: "[name].js",
         assetFileNames: "[name].[ext]",
       }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
     }
   }
 })
